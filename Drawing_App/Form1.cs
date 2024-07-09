d47f4f25bbd70bd8f0123d73d81e24155f85b84a
Oliver Quaye <129207321+oliverquaye23@users.noreply.github.com>
2024-07-09 20:24:55 +0000

    Drawing_app

---
+++ Drawing_App/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace MyDrawingApp
+{
+    public partial class Form1 : Form
+    {
+        private bool isDrawing;
+        private Point startPoint;
+
+        public Form1()
+        {
+            InitializeComponent();
+            drawingPanel.MouseDown += DrawingPanel_MouseDown;
+            drawingPanel.MouseMove += DrawingPanel_MouseMove;
+            drawingPanel.MouseUp += DrawingPanel_MouseUp;
+        }
+
+        private void DrawingPanel_MouseDown(object sender, MouseEventArgs e)
+        {
+            isDrawing = true;
+            startPoint = e.Location;
+        }
+
+        private void DrawingPanel_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isDrawing)
+            {
+                using (Graphics g = drawingPanel.CreateGraphics())
+                {
+                    g.DrawLine(Pens.Black, startPoint, e.Location);
+                }
+                startPoint = e.Location;
+            }
+        }
+
+        private void DrawingPanel_MouseUp(object sender, MouseEventArgs e)
+        {
+            isDrawing = false;
+        }
+
+        private void drawingPanel_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
