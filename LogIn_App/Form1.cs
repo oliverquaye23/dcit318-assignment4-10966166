72887193586ec178985f9584c29e6a06dfa7a769
Oliver Quaye <129207321+oliverquaye23@users.noreply.github.com>
2024-07-09 20:26:06 +0000

    Login_App

---
+++ LogIn_App/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace MyLoginApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            string username = txtUsername.Text;
+            string password = txtPassword.Text;
+            MessageBox.Show($"Username: {username}\nPassword: {password}", "Login Information");
+        }
+    }
+}
